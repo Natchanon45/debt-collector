263d83faaab0e3873f7d8e99166f70a333dec556
2f-ณัฐ <natchanon@2fellows.com>
2026-06-13 13:13:20 +0700

    update app version 9.0.5

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,11 @@
 export const APP_INFO = {
-    version: '9.0.4',
+    version: '9.0.5',
     authorized: 'นายณัฐชนน ศรีเปล่ง',
     year: new Date().getFullYear()
 };
 
 export const APP_VERSION = APP_INFO.version;
-export const LS = 'debt_collector_phase3_v9.0.4';
+export const LS = 'debt_collector_phase3_v9.0.5';
 
 export const blank = {
     debtors: [],
