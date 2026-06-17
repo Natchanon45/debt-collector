3d55ab218e7a34c9753b9a57d02ccf9d7d7004e6
“Natchanon45” <sripleng.natchanon@gmail.com>
2026-06-17 11:06:19 +0700

    udpate app version 9.7.8

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,11 @@
 export const APP_INFO = {
-    version: '9.7.6',
+    version: '9.7.8',
     authorized: 'นายณัฐชนน ศรีเปล่ง',
     year: new Date().getFullYear()
 };
 
 export const APP_VERSION = APP_INFO.version;
-export const LS = 'debt_collector_phase3_v9.7.6';
+export const LS = 'debt_collector_phase3_v9.7.8';
 
 export const blank = {
     debtors: [],
