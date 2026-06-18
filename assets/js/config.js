ea9a80d20f0cb88fc674cd4aef413af2b58353a4
Natchanon45 <103320884+Natchanon45@users.noreply.github.com>
2026-06-18 23:04:04 +0700

    Bump version to 9.9.3

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,13 +1,13 @@
 import './generic-a4-unified.js';
 
 export const APP_INFO = {
-    version: '9.8.6',
+    version: '9.9.3',
     authorized: 'นายณัฐชนน ศรีเปล่ง',
     year: new Date().getFullYear()
 };
 
 export const APP_VERSION = APP_INFO.version;
-export const LS = 'debt_collector_phase3_v9.8.6';
+export const LS = 'debt_collector_phase3_v9.9.3';
 
 export const blank = {
     debtors: [],
